a8a3f5c587fafdfa66b55fce327eb5a774290848
Saheed Ansari <shahidansari531@gmail.com>
2025-04-09 06:11:25 +0530

    lengthOfLongestSubstring

---
+++ src/Main.kt
@@ -0,0 +1,21 @@
+fun main() {
+    println(lengthOfLongestSubstring("abcabcbb"))
+}
+
+fun lengthOfLongestSubstring(s: String): Int {
+    val seen = mutableSetOf<Char>()
+    var left = 0
+    var maxLength = 0
+    for (right in s.indices) {
+        while (s[right] in seen){
+            seen.remove(s[left])
+            left++
+        }
+        seen.add(s[right])
+        maxLength = maxOf(maxLength,right-left+1)
+    }
+    return maxLength
+}
+
+
+
