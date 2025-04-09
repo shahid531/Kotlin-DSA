38738bcec72a881ca1a2d23a721b1e1ae996fb45
Saheed Ansari <shahidansari531@gmail.com>
2025-04-09 06:24:40 +0530

    reverseString

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,13 @@
 fun main() {
-    println(lengthOfLongestSubstring("abcabcbb"))
+    println(reverseString("hello"))
 }
 
-fun lengthOfLongestSubstring(s: String): Int {
-    val seen = mutableSetOf<Char>()
-    var left = 0
-    var maxLength = 0
-    for (right in s.indices) {
-        while (s[right] in seen){
-            seen.remove(s[left])
-            left++
-        }
-        seen.add(s[right])
-        maxLength = maxOf(maxLength,right-left+1)
+fun reverseString(s: String): String {
+    var output = ""
+    for (i in s.length - 1 downTo 0) {
+        output += s[i]
     }
-    return maxLength
+    return output
 }
 
 
